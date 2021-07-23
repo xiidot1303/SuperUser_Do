f4454b971d39116fd7986bdea44bcf3642800ea8
xiidot1303 <xii1303@inbox.ru>
2021-07-23 17:27:17 +0500

    done ignore

--- python/sending sms/sending sms.py
+++ python/sending sms/sending sms.py
@@ -1,16 +1,19 @@
 import os
 from twilio.rest import Client
-
+from dotenv import load_dotenv
 # Find your Account SID and Auth Token at twilio.com/console
 
-account_sid = 'TWILIO_ACCOUNT_SID' # your SID from twilio
-auth_token = 'TWILIO_AUTH_TOKEN'  # your TOKEN from twilio
+# Set in .env file and get it by dotenv
+basedir = os.path.abspath(os.path.dirname(''))
+load_dotenv(os.path.join(basedir, '.env'))
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID') # your SID from twilio
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')  # your TOKEN from twilio
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Heyyyyy',
                               from_='+12135169316',
-                              to='+998990920260'
+                              to='+998990920261'
                           )
 
 print(message.sid)
