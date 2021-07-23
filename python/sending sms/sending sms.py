28f254a496008e056a4fa41d87310199f54fc19c
xiidot1303 <xii1303@inbox.ru>
2021-07-23 17:10:14 +0500

    done ignore

--- python/sending sms/sending sms.py
+++ python/sending sms/sending sms.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 
-account_sid = 'TWILIO_ACCOUNT_SID' # your SID from twilio
-auth_token = 'TWILIO_AUTH_TOKEN'  # your TOKEN from twilio
+account_sid = 'AC42b701d54ce18efcddc8d234249365b0' # your SID from twilio
+auth_token = '206da90b9c94bd7505d71a4c2cd5c16a'  # your TOKEN from twilio
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
